3c2b2bb8daaa30c499b6ba6580ecea72506d4581
Lucas Pickering <lucaspickering9@gmail.com>
2018-05-26 18:26:53 -0400

    Overview: Reduce number of React elements created

---
+++ frontend/src/components/EventMarks.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import BtPropTypes from '../util/BtPropTypes';
+import RosterPalette from '../util/RosterPalette';
+
+class EventMarks extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      mouseOver: null, // Null or mark ID
+    };
+  }
+
+  render() {
+    const { marks, scale, rosterPalette } = this.props;
+
+    return marks.map(({
+      id,
+      icon: { code: iconCode, ...iconProps },
+      pos: { x, y },
+      player,
+    }) => (
+      <g
+        key={id}
+        fill={player ? rosterPalette.getPlayerColor(player.id) : undefined}
+        transform={`translate(${x},${y}),scale(${scale})`}
+        onMouseEnter={() => this.setState({ mouseOver: id })}
+        onMouseLeave={() => this.setState({ mouseOver: null })}
+      >
+        <text className="fa" {...iconProps}>{iconCode}</text>
+      </g>
+    ));
+  }
+}
+
+EventMarks.propTypes = {
+  marks: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    icon: PropTypes.element.isRequired,
+    pos: BtPropTypes.pos.isRequired,
+    time: PropTypes.number.isRequired,
+    player: PropTypes.objectOf(PropTypes.any),
+  })).isRequired,
+  scale: PropTypes.number.isRequired,
+  rosterPalette: PropTypes.instanceOf(RosterPalette).isRequired,
+};
+
+export default EventMarks;
