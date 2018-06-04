0084244ef723ac857be33b4c1560b3d222fe90f0
Lucas Pickering <lucaspickering9@gmail.com>
2018-06-04 12:29:00 -0400

    Overview: temporary grid rendering

--- frontend/src/components/GameMap.jsx
+++ frontend/src/components/GameMap.jsx
@@ -2,17 +2,62 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { ReactSVGPanZoom } from 'react-svg-pan-zoom';
 
-import { mapImage } from '../util/funcs';
+import { mapImage, range } from '../util/funcs';
 import BtPropTypes from '../util/BtPropTypes';
+
+const GRID_SMALL_STEP = 100;
+const GRID_LARGE_STEP = 1000;
+
+const GridLine = ({
+  d,
+  mapSize,
+  major,
+  vertical,
+  ...rest
+}) => (
+  <path
+    d={vertical ? `M${d},0 V${mapSize} Z` : `M0,${d} H${mapSize} Z`}
+    stroke="#666"
+    strokeWidth={major ? 5 : 2}
+    {...rest}
+  />
+);
+
+GridLine.propTypes = {
+  d: PropTypes.number.isRequired,
+  mapSize: PropTypes.number.isRequired,
+  major: PropTypes.bool,
+  vertical: PropTypes.bool,
+};
+
+GridLine.defaultProps = {
+  major: false,
+  vertical: false,
+};
 
 class GameMap extends React.PureComponent {
   componentDidMount() {
     this.viewer.fitToViewer();
   }
 
+  renderGrid() {
+    const { size } = this.props.map;
+    const lines = range(GRID_SMALL_STEP, size, GRID_SMALL_STEP);
+    return lines.map(d => {
+      const major = (d % GRID_LARGE_STEP === 0);
+      return (
+        <g>
+          <GridLine d={d} mapSize={size} major={major} />
+          <GridLine d={d} mapSize={size} major={major} vertical />
+        </g>
+      );
+    });
+  }
+
   render() {
     const {
       map: { name, size },
+      showGrid,
       children,
       ...rest
     } = this.props;
@@ -26,6 +71,7 @@ class GameMap extends React.PureComponent {
       >
         <svg width={size} height={size}>
           <image href={mapImage(name)} width={size} height={size} />
+          {showGrid && this.renderGrid()}
           {children}
         </svg>
       </ReactSVGPanZoom>
@@ -35,10 +81,12 @@ class GameMap extends React.PureComponent {
 
 GameMap.propTypes = {
   map: BtPropTypes.map.isRequired,
+  showGrid: PropTypes.bool,
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.any)]),
 };
 
 GameMap.defaultProps = {
+  showGrid: false,
   children: [],
 };
 
