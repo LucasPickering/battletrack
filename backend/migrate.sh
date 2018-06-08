cca4fca219bbb7100e846f9d7085960f9bd2f382
Lucas Pickering <lucas@lucaspickering.me>
2018-06-08 15:38:50 -0400

    Make migrate.sh work on Mac

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -5,5 +5,12 @@ set -e -x
 rm btcore/migrations/0*.py
 rm telemetry/migrations/0*.py
 python3 manage.py makemigrations
-sudo -u postgres psql -v 'ON_ERROR_STOP=1' -c 'DROP DATABASE battletrack;' -c 'CREATE DATABASE battletrack;' -c 'GRANT ALL PRIVILEGES ON DATABASE battletrack TO btuser;'
+case "$(uname)" in
+    "Linux" )
+        sudo -u postgres psql -v 'ON_ERROR_STOP=1' -c 'DROP DATABASE battletrack;' -c 'CREATE DATABASE battletrack;' -c 'GRANT ALL PRIVILEGES ON DATABASE battletrack TO btuser;'
+    ;;
+    "Darwin" )
+        psql -d postgres -v 'ON_ERROR_STOP=1' -c 'DROP DATABASE battletrack;' -c 'CREATE DATABASE battletrack;' -c 'GRANT ALL PRIVILEGES ON DATABASE battletrack TO btuser;'
+    ;;
+esac
 python3 manage.py migrate
