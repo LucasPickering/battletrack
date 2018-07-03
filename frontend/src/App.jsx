b8c4febae6857bae166a402a496ea203ff84dd61
Lucas Pickering <lucas@lucaspickering.me>
2018-07-02 21:47:11 -0400

    Restyle 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import BtRoute from './components/BtRoute';
 import Home from './components/Home';
 import Match from './components/Match';
 import MatchOverview from './components/MatchOverview';
 import Player from './components/Player';
+import NotFound from './components/NotFound';
 import './styles/App.css';
-
-const NotFound = () => (
-  <h1 style={{ textAlign: 'center' }}>404 - Not Found</h1>
-);
 
 
 const App = () => (
@@ -20,7 +17,7 @@ const App = () => (
       <BtRoute exact path="/matches/:matchId" component={Match} />
       <BtRoute exact path="/matches/:matchId/overview" component={MatchOverview} fullscreen />
       <BtRoute exact path="/players/:shard/:playerName" component={Player} />
-      <BtRoute component={NotFound} />
+      <Route component={NotFound} />
     </Switch>
   </Router>
 );
---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../styles/NotFound.css';
+
+const NotFound = () => (
+  <div className="not-found">
+    <h1>Not Found</h1>
+    <h3>No Bueno!</h3>
+  </div>
+);
+
+export default NotFound;
