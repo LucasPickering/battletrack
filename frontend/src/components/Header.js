cda0a754c17027bb4d105fd450ff2f0239ab0015
Lucas Pickering <lucas@lucaspickering.me>
2018-08-14 20:33:16 -0400

    Add Battletrack/beta to title bar
    Closes #18

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,6 +5,8 @@ import 'styles/Header.css';
 
 const Header = () => (
   <div className="bt-header">
+    <p className="bt-logo">Battletrack</p>
+    <p className="beta-tag">BETA</p>
     <PlayerSearch />
   </div>
 );
