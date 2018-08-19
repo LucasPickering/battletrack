9a4335d5024d46bb65bbfe7476f76335a66e79e4
Lucas Pickering <lucas@lucaspickering.me>
2018-08-18 22:02:40 -0400

    Reconfigure assets (again) to copy from submodule
    Gitlab no longer has to clone submodules to run the pipeline.

--- frontend/assets.py
+++ frontend/assets.py
@@ -1,38 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import os
 import shutil
 import subprocess
 from PIL import Image
 
-ASSETS_DIR = 'src/api-assets'
-
-# Maps
-MAP_SRC_DIR = os.path.join(ASSETS_DIR, 'Assets/Maps/')
-MAP_FILE_NAMES = {
-    # These file names are stupid
-    'Erangel_Main': 'Erangel_Main_High_Res.jpg',
-    'Desert_Main': 'Miramar_Main_High_Res.jpg',
-    'Savage_Main': 'Savage_Main_High_Res.png',
-}
-TILE_MAP_DEST_DIR = 'public/assets/maps'
-TILE_SIZE = 512
-ZOOM_LEVELS = 5
-
+ASSETS_MANIFEST = 'assets-manifest.json'
 DELETE_PROMPT = "{path} already exists, do you want to delete it?"
 
 
-def confirm_prompt(prompt, padding=0, default_yes=True):
-    if args.yes:
-        return True
-    padding_str = ' ' * padding
-    response = input(f"{padding_str}{prompt} [{'Y/n' if default_yes else 'y/N'}] ")
-    return response.startswith('y') or (not response.startswith('n') and default_yes)
-
-
-def pull_latest(**kwargs):
-    os.chdir(ASSETS_DIR)
+def pull_latest(assets_manifest, **kwargs):
+    os.chdir(assets_manifest['api_assets_dir'])
     old_commit = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
     subprocess.run(['git', 'pull', 'origin', 'master'])
     diff = subprocess.check_output(['git', 'diff', '--name-only', old_commit]).decode()
@@ -40,13 +20,37 @@ def pull_latest(**kwargs):
     print(diff)
 
 
-def tile_for_all_zooms(image_path, output_dir):
-    print(f"Tiling {image_path} => {output_dir}")
+def copy_static_assets(assets_dir, static_cfg):
+    output_dir = static_cfg['output_dir']
+    for file in static_cfg['files']:
+        src = os.path.join(assets_dir, file)
+        dest = os.path.join(output_dir, file)
 
-    if os.path.exists(output_dir) and not confirm_prompt(DELETE_PROMPT.format(path=output_dir),
-                                                         padding=2):
-        print(f"  Skipping {image_path}")
-        return
+        # Make the destination dir if necessary
+        dest_dir, _ = os.path.split(dest)
+        os.makedirs(dest_dir, exist_ok=True)
+
+        print(f"Copying {src} => {dest}")
+        shutil.copyfile(src, dest)
+
+
+def tile_image(image, output_dir, tile_size):
+    width, height = image.size
+    for x in range(0, width, tile_size):
+        for y in range(0, height, tile_size):
+            box = (x, y, x + tile_size, y + tile_size)  # The portion of the parent image to crop
+
+            # Build a path for the new file
+            tile_x = x // tile_size
+            tile_y = y // tile_size
+            tile_path = os.path.join(output_dir, f'{tile_x}_{tile_y}.jpg')
+
+            cropped = image.crop(box)
+            cropped.save(tile_path, quality=95, subsampling=0)  # No compression
+
+
+def tile_for_all_zooms(image_path, output_dir, zoom_levels, tile_size):
+    print(f"Tiling {image_path} => {output_dir}")
 
     # Delete the directory (if present) and re-make it
     try:
@@ -59,38 +63,32 @@ def tile_for_all_zooms(image_path, output_dir):
     # General approach: cut the full-res image into uniform tiles, downscale the full image by
     # 50%, then repeat until the image is only one tile
     image = Image.open(image_path)
-    for zoom in reversed(range(ZOOM_LEVELS)):
+    for zoom in reversed(range(zoom_levels)):
         zoom_output_dir = os.path.join(output_dir, str(zoom))
         os.makedirs(zoom_output_dir)
 
-        tile_image(image, zoom_output_dir)  # Generate tiles
+        tile_image(image, zoom_output_dir, tile_size)  # Generate tiles
 
         width, height = image.size
         image = image.copy()
         image.thumbnail((width // 2, height // 2))
 
 
-def tile_image(image, output_dir):
-    width, height = image.size
-    for x in range(0, width, TILE_SIZE):
-        for y in range(0, height, TILE_SIZE):
-            box = (x, y, x + TILE_SIZE, y + TILE_SIZE)  # The portion of the parent image to crop
-
-            # Build a path for the new file
-            tile_x = x // TILE_SIZE
-            tile_y = y // TILE_SIZE
-            tile_path = os.path.join(output_dir, f'{tile_x}_{tile_y}.jpg')
-
-            cropped = image.crop(box)
-            cropped.save(tile_path, quality=95, subsampling=0)  # No compression
+def tile_maps(assets_dir, maps_cfg):
+    input_dir = os.path.join(assets_dir, maps_cfg['input_dir'])
+    output_dir = maps_cfg['output_dir']
+    zoom_levels = maps_cfg['zoom_levels']
+    tile_size = maps_cfg['tile_size']
+    for map_name, file_name in maps_cfg['files'].items():
+        tile_for_all_zooms(os.path.join(input_dir, file_name),
+                           os.path.join(output_dir, map_name),
+                           zoom_levels, tile_size)
 
 
-def tile_all(output_dir, **kwargs):
-    # Find all the map files we care about and tile each one
-    files = {map_name: os.path.join(MAP_SRC_DIR, fname)
-             for map_name, fname in MAP_FILE_NAMES.items()}
-    for map_name, file_name in files.items():
-        tile_for_all_zooms(file_name, os.path.join(output_dir, map_name))
+def update_all(assets_manifest, **kwargs):
+    assets_dir = assets_manifest['api_assets_dir']
+    copy_static_assets(assets_dir, assets_manifest['static'])
+    tile_maps(assets_dir, assets_manifest['maps'])
 
 
 if __name__ == '__main__':
@@ -102,10 +100,13 @@ if __name__ == '__main__':
     pull_parser = subparsers.add_parser('pull', help="Pull the latest version of the assets repo")
     pull_parser.set_defaults(func=pull_latest)
 
-    tile_parser = subparsers.add_parser('tile', help="Break map images into tiles")
-    tile_parser.add_argument('--output-dir', '-o', default=TILE_MAP_DEST_DIR,
-                             help="Directory to output tiled maps to")
-    tile_parser.set_defaults(func=tile_all)
+    update_parser = subparsers.add_parser('update', help="Copy/process asset files into the repo")
+    update_parser.set_defaults(func=update_all)
+
+    # tile_parser = subparsers.add_parser('tile', help="Break map images into tiles")
+    # tile_parser.add_argument('--output-dir', '-o', default=TILE_MAP_DEST_DIR,
+    #                          help="Directory to output tiled maps to")
+    # tile_parser.set_defaults(func=tile_all)
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
     os.chdir(script_dir)
@@ -113,4 +114,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     argd = vars(args)
     func = argd.pop('func')
-    func(**argd)  # Run the function for the action
+
+    # Load manifest file
+    with open(ASSETS_MANIFEST) as f:
+        assets_manifest = json.load(f)
+
+    func(assets_manifest=assets_manifest, **argd)  # Run the function for the action
