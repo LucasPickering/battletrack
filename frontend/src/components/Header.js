9296bf3a4c0a119d273321500383a94e17b86043
Lucas Pickering <lucas@lucaspickering.me>
2018-11-15 22:08:49 -0500

    Add GitLab link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,6 +8,15 @@ const Header = () => (
     <p className="bt-logo">Battletrack</p>
     <p className="beta-tag">BETA</p>
     <PlayerSearch />
+    <a
+      className="gitlab-icon"
+      href="https://gitlab.com/LucasPickering/battletrack"
+    >
+      <img
+        src="https://gitlab.com/gitlab-com/gitlab-artwork/raw/master/logo/logo-square.svg"
+        alt="GitLab"
+      />
+    </a>
   </div>
 );
 
